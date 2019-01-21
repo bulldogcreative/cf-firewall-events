4c82ccce50be5c04fc6d372ce25b7bc82fb20166
Levi Durfee <levi.durfee@gmail.com>
2019-01-20 20:59:19 -0500

    Add option for additional parameters

--- src/Firewall.php
+++ src/Firewall.php
@@ -18,16 +18,18 @@ class Firewall
 
     /**
      * @param string $zoneIdentifier
+     * @param array $parameters
      * @return \Psr\Http\Message\ResponseInterface
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function events(string $zoneIdentifier): ResponseInterface
+    public function events(string $zoneIdentifier, array $parameters = []): ResponseInterface
     {
         return $this->client->request(
             'GET', 
             'zones/'.$zoneIdentifier.'/security/events',
             [
                 'debug' => $this->debug,
+                'query' => $parameters,
             ]
         );
     }
