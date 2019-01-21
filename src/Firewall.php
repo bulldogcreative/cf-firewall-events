6d559766c430d82d24fd70ed3a2476ac71eb3df6
Levi Durfee <levi.durfee@gmail.com>
2019-01-20 21:01:49 -0500

    Describe methods

--- src/Firewall.php
+++ src/Firewall.php
@@ -16,6 +16,9 @@ class Firewall
     }
 
     /**
+     * Logs of the mitigations performed by Firewall features.
+     *
+     * @see https://api.cloudflare.com/#firewall-events-list-events
      * @param string $zoneIdentifier
      * @param array $parameters
      * @return \Psr\Http\Message\ResponseInterface
@@ -34,6 +37,8 @@ class Firewall
     }
 
     /**
+     * Set to true to enable debug output with the handler used to send a request.
+     *
      * @param bool $debug
      */
     public function setDebug(bool $debug)
